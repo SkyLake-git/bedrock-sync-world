7ef9c170fd8131a9c65e3f07da09a40bbbc6aa76
SkyLake <70795425+SkyLake-git@users.noreply.github.com>
2023-02-12 10:22:20 +0900

    update

---
+++ index.d.ts
@@ -0,0 +1,56 @@
+import { Vec3 } from "vec3"
+import { Block } from 'prismarine-block'
+import { BedrockChunk, CommonChunk, ExtendedBlock } from 'prismarine-chunk'
+import { EventEmitter } from 'events'
+import { Entity, EntityType } from "prismarine-entity"
+
+export type ChunkCoord = number // >> 4 shifted
+export type ChunkPosition = {
+	x: ChunkCoord,
+	z: ChunkCoord
+}
+
+export interface WorldBlockDataProvider {
+	getBlock(position: Vec3, full?: boolean): Block
+}
+
+export interface IChunkGenerator {
+	generate(x: ChunkCoord, z: ChunkCoord, chunkType: typeof BedrockChunk): BedrockChunk
+}
+
+export class EmptyChunkGenerator implements IChunkGenerator {
+	generate(x: number, z: number, chunkType: typeof BedrockChunk): BedrockChunk
+}
+
+export class World extends EventEmitter implements WorldBlockDataProvider {
+
+	chunks: BedrockChunk[]
+	_entities: Map<number, Entity>
+	_players: Map<number, Entity>
+
+	_defaultChunkGenerator: IChunkGenerator
+
+	constructor(defaultChunkGenerator: IChunkGenerator)
+
+	addEntity(entity: Entity): void
+
+	getEntity(id: number): Entity | null
+
+	getPlayer(id: number): Entity | null
+
+	toChunkCoord(v: number): ChunkCoord
+
+	toChunkPosition(v: { x: number, y: number }): ChunkPosition
+
+	getChunkHash(x: ChunkCoord, z: ChunkCoord): number
+
+	getChunkFrom(x: number, z: number): BedrockChunk
+
+	getChunk(x: ChunkCoord, z: ChunkCoord): BedrockChunk
+
+	_setChunk(x: ChunkCoord, z: ChunkCoord, chunk: BedrockChunk, overwrite?: boolean): void
+
+	getBlock(position: Vec3, full?: boolean): Block
+
+	setBlock(position: Vec3, block: ExtendedBlock): void
+}
---
+++ index.js
@@ -0,0 +1,100 @@
+"use strict";
+var __extends = (this && this.__extends) || (function () {
+    var extendStatics = function (d, b) {
+        extendStatics = Object.setPrototypeOf ||
+            ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
+            function (d, b) { for (var p in b) if (Object.prototype.hasOwnProperty.call(b, p)) d[p] = b[p]; };
+        return extendStatics(d, b);
+    };
+    return function (d, b) {
+        if (typeof b !== "function" && b !== null)
+            throw new TypeError("Class extends value " + String(b) + " is not a constructor or null");
+        extendStatics(d, b);
+        function __() { this.constructor = d; }
+        d.prototype = b === null ? Object.create(b) : (__.prototype = b.prototype, new __());
+    };
+})();
+exports.__esModule = true;
+exports.World = exports.EmptyChunkGenerator = void 0;
+var morton_1 = require("morton");
+var data_registries_1 = require("data-registries");
+var events_1 = require("events");
+var EmptyChunkGenerator = /** @class */ (function () {
+    function EmptyChunkGenerator() {
+    }
+    EmptyChunkGenerator.prototype.generate = function (x, z, chunkType) {
+        return new chunkType({ x: x, z: z });
+    };
+    return EmptyChunkGenerator;
+}());
+exports.EmptyChunkGenerator = EmptyChunkGenerator;
+var World = /** @class */ (function (_super) {
+    __extends(World, _super);
+    function World(defaultChunkGenerator) {
+        var _this = _super.call(this) || this;
+        _this.chunks = [];
+        _this._entities = new Map();
+        _this._players = new Map();
+        _this._defaultChunkGenerator = defaultChunkGenerator;
+        return _this;
+    }
+    World.prototype.addEntity = function (entity) {
+        this._entities.set(entity.id, entity);
+        if (entity.type == 'player') {
+            this._players.set(entity.id, entity);
+        }
+        this.emit("entity_add", entity);
+    };
+    World.prototype.getEntity = function (id) {
+        var _a;
+        return (_a = this._entities.get(id)) !== null && _a !== void 0 ? _a : null;
+    };
+    World.prototype.getPlayer = function (id) {
+        var _a;
+        return (_a = this._players.get(id)) !== null && _a !== void 0 ? _a : null;
+    };
+    World.prototype.toChunkCoord = function (v) {
+        return Math.floor(v) >> 4;
+    };
+    World.prototype.toChunkPosition = function (v) {
+        return { x: this.toChunkCoord(v.x), z: this.toChunkCoord(v.z) };
+    };
+    World.prototype.getChunkHash = function (x, z) {
+        return (0, morton_1["default"])(x, z);
+    };
+    World.prototype.getChunkFrom = function (x, z) {
+        return this.getChunk(this.toChunkCoord(x), this.toChunkCoord(z));
+    };
+    World.prototype.getChunk = function (x, z) {
+        var hash = this.getChunkHash(x, z);
+        var chunk = this.chunks[hash];
+        if (chunk == null) {
+            var newChunk = this._defaultChunkGenerator.generate(x, z, data_registries_1.latestRegistries.chunk);
+            this._setChunk(x, z, newChunk);
+            return newChunk;
+        }
+        return chunk;
+    };
+    World.prototype._setChunk = function (x, z, chunk, overwrite) {
+        var hash = this.getChunkHash(x, z);
+        if (!overwrite && this.chunks[hash]) {
+            throw Error("cannot overwrite");
+        }
+        this.chunks[hash] = chunk;
+    };
+    World.prototype.getBlock = function (position, full) {
+        var floorPos = position.floor();
+        var chunk = this.getChunkFrom(floorPos.x, floorPos.y);
+        return chunk.getBlock(floorPos, full);
+    };
+    World.prototype.setBlock = function (position, block) {
+        var floorPos = position.floor();
+        block.position = floorPos.clone();
+        var chunk = this.getChunkFrom(floorPos.x, floorPos.y);
+        var oldBlock = chunk.getBlock(floorPos);
+        chunk.setBlock(floorPos, block);
+        this.emit("block_changed", chunk, block, oldBlock);
+    };
+    return World;
+}(events_1.EventEmitter));
+exports.World = World;
